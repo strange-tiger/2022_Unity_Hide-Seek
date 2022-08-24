5ff32cdadbad7f2e5c2bc4d6c62eae1146b6573d
strange-tiger <103083761+strange-tiger@users.noreply.github.com>
2022-08-24 12:51:09 +0900

    Feat: HowToPlay 추가
    버튼으로 토글

--- Hide-Seek/Assets/Scripts/UI/TitleUI.cs
+++ Hide-Seek/Assets/Scripts/UI/TitleUI.cs
@@ -7,21 +7,29 @@ public class TitleUI : MonoBehaviour
 {
     private Button _startBtn;
     private Button _quitBtn;
+    private Button _howBtn;
+    private GameObject _howPanel;
     private void Awake()
     {
         _startBtn = transform.GetChild(0).GetComponent<Button>();
         _quitBtn = transform.GetChild(1).GetComponent<Button>();
+        _howBtn = transform.GetChild(2).GetComponent<Button>();
+        _howPanel = transform.GetChild(4).gameObject;
+        _howPanel.SetActive(false);
 
         _startBtn.onClick.AddListener(start);
         _quitBtn.onClick.AddListener(Quit);
+        _howBtn.onClick.AddListener(ShowHowToPlay);
     }
 
     public void start() => GameManager.Instance.start();
     public void Quit() => GameManager.Instance.Quit();
+    public void ShowHowToPlay() => _howPanel.SetActive(!_howPanel.activeSelf);
 
     private void OnDisable()
     {
         _startBtn.onClick.RemoveListener(start);
         _quitBtn.onClick.RemoveListener(Quit);
+        _howBtn.onClick.RemoveListener(ShowHowToPlay);
     }
 }
